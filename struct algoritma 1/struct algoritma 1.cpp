7885ad810d2215d87226133aafe9596364808b0c
zil-web <126854462+zil-web@users.noreply.github.com>
2023-04-05 10:34:05 +0700

    memasukan string

--- struct algoritma 1/struct algoritma 1.cpp
+++ struct algoritma 1/struct algoritma 1.cpp
@@ -7,20 +7,5 @@ struct mahasiswa
 	string nama;
 	string alamat;
 	int umur;
-
 };
 
-int main() {
-	mahasiswa mhs, mhs2;
-
-	mhs.nim = "20210140073";
-	mhs.umur = 19;
-	mhs.nama = "aagil imam";
-	mhs.alamat = "riau";
-
-	cout << "masukan NIMM ; ";
-	cin >> mhs2.nim;
-	cout << "masukan umur : ";
-	cin >> mhs2.umur;
-
-};
